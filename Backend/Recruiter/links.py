2daca1813917a0b57efb4169b54a7f58b7774e81
bbahd30 <b_bonal@cs.iitr.ac.in>
2022-09-25 00:51:09 +0530

    Authenticate members

---
+++ Backend/Recruiter/links.py
@@ -0,0 +1,5 @@
+client_data = {
+    "client_id": "LltYOkhVLLTdB1iWtRe0OcfHJwQhvc2faPM9eUGv",
+
+    "client_secret_key":"4xOGnX2did9HPy6XQ92cS6ATInhe0om7GEoYu2bA9YPiOufQ1w9xaJ5QWFkCCX5tc6Bbq9de16TMe6Sh38uGrzbrmgv4J5hAZJFUe4atIkTvzR0jvah8mhZT0YweuqKi"
+}
